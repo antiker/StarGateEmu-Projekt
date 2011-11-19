7549c8cbe3fb3f3c959936c48df7fcc6aa7db3b0
antiker <krallekalle@web.de>
2011-11-19 10:06:49 +0100

    DB/Update 5.1.6

---
+++ SQL_Updates/World/5.1.6 Update Pack.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `ScriptName`='boss_toxitron' WHERE `entry`=42180;
+UPDATE `creature_template` SET `ScriptName`='boss_magmatron' WHERE `entry`=42178;
+UPDATE `creature_template` SET `ScriptName`='boss_arcanotron' WHERE `entry`=42166;
+UPDATE `creature_template` SET `ScriptName`='boss_electron' WHERE `entry`=42179;
