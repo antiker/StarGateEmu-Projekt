1926e4672ee977bf9cc4d145fe00294a1328c7ad
antiker <krallekalle@web.de>
2011-11-27 15:42:28 +0100

    Core/Quest: Fix

--- src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
+++ src/server/scripts/EasternKingdoms/ScarletEnclave/chapter1.cpp
@@ -1,25 +1,5 @@
 /*
- * Copyright (C) 2005-2011 MaNGOS <http://www.getmangos.com/>
- *
- * Copyright (C) 2008-2011 Trinity <http://www.trinitycore.org/>
- *
- * Copyright (C) 2006-2011 ScriptDev2 <http://www.scriptdev2.com/>
- *
- * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * Copyright (C) 2010-2011 Project StarGate
  */
 
 #include "ScriptPCH.h"
@@ -756,7 +736,7 @@ public:
                         caster->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_SPELLCLICK);
                         caster->setFaction(35);
                         DoCast(caster, CALL_DARK_RIDER, true);
-                        if (Creature* Dark_Rider = me->FindNearestCreature(28654, 15))
+                        if (Creature* Dark_Rider = me->FindNearestCreature(28654, 15.0f, true))
                             CAST_AI(npc_dark_rider_of_acherus::npc_dark_rider_of_acherusAI, Dark_Rider->AI())->InitDespawnHorse(caster);
                     }
                 }
