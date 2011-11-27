57b97ea167c66b10564de200bc9d524abfb93f36
antiker <krallekalle@web.de>
2011-11-27 20:16:57 +0100

    Core/Skript Fix

--- SQL_Updates/World/5.1.8 Update Pack.sql
+++ SQL_Updates/World/5.1.8 Update Pack.sql
@@ -3,4 +3,5 @@ UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_sinestra' WHERE
 UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_assembly_of_ascendents' WHERE `entry`=43687;
 UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_theralion_and_valiona' WHERE `entry`=45993;
 UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_atramedes' WHERE `entry`=41442;
+UPDATE `creature_template` SET `AIName` = '', `ScriptName`='boss_halfus_wyrmbreaker' WHERE `entry`=44600;
 
--- src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_halfus_wyrmbreaker.cpp
+++ src/server/scripts/EasternKingdoms/BastionOfTwilight/boss_halfus_wyrmbreaker.cpp
@@ -1,5 +1,20 @@
 /*
- * Copyright (C) 2010-2011 Project StarGate
+ * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2010-2011 MigCore <http://wow-mig.ru/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ScriptPCH.h"
@@ -54,9 +69,9 @@ public:
         return new boss_halfus_wyrmbreakerAI (creature);
     }
 
-    struct boss_halfus_wyrmbreakerAI : public ScriptedAI
+    struct boss_halfus_wyrmbreakerAI : public BossAI
     {
-        boss_halfus_wyrmbreakerAI(Creature* creature) : ScriptedAI(creature)
+        boss_halfus_wyrmbreakerAI(Creature* creature) : BossAI(creature, DATA_HALFUS)
         {
             instance = creature->GetInstanceScript();
         }
@@ -104,6 +119,7 @@ public:
 
         void JustDied(Unit* /*Killer*/)
         {
+			_JustDied();
             instance->SetData(DATA_HALFUS, DONE);
         }
 
